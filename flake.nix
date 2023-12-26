a29d9d429068f0f3a23951bc592ccd48fa0e9aa7
João Augusto Lissoni Lanjoni <joaoaugustolanjoni@gmail.com>
2023-12-26 17:46:51 -0300

    fix: flake description

--- flake.nix
+++ flake.nix
@@ -1,18 +1,16 @@
 {
-  description = "my minimal flake";
+  description = "snowflake config for guto's macbook pro";
+  
   inputs = {
-    # Where we get most of our software. Giant mono repo with recipes
-    # called derivations that say how to build software.
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; # nixos-23.11
 
-    # Manages configs links things into your home directory
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # Controls system level software and settings including fonts
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
+
   outputs = inputs@{ nixpkgs, home-manager, darwin, ... }: {
     darwinConfigurations.artemis = darwin.lib.darwinSystem {
       system = "aarch64-darwin";
