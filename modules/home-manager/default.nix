25ab270c51b5eb26e52591cb7c9dc5117a2a059a
João Augusto Lissoni Lanjoni <joaoaugustolanjoni@gmail.com>
2023-12-29 23:01:26 -0300

    fix: home-manager base settings

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,44 +1,60 @@
 { pkgs, ... }: {
-  # Don't change this when you change package input. Leave it alone.
-  home.stateVersion = "23.11";
   # specify my home-manager configs
-  home.packages = with pkgs; [
-    ripgrep
-    fd
-    curl
-    less
-    neofetch
-    neovim
-    kubectl
-    kind
-    httpie
-    clojure
-    clojure-lsp
-    leiningen
-    htop
-    pfetch
-    crystal
-  ];
-  home.sessionVariables = {
-    PAGER = "less";
-    CLICLOLOR = 1;
-    EDITOR = "nvim";
-  };
-
   imports = [
     ./settings/zsh.nix
     ./settings/kitty.nix
     ./settings/tmux.nix
   ];
 
-  programs.bat.enable = true;
-  programs.bat.config.theme = "TwoDark";
-  programs.fzf.enable = true;
-  programs.fzf.enableZshIntegration = true;
-  programs.eza.enable = true;
-  programs.git.enable = true;
-  programs.starship.enable = true;
-  programs.starship.enableZshIntegration = true;
-  
+  home = {
+    username = "guto";
+    homeDirectory = "/Users/guto";
+    packages = with pkgs; [
+      ripgrep
+        fd
+        curl
+        less
+        neofetch
+        neovim
+        kubectl
+        kind
+        httpie
+        clojure
+        clojure-lsp
+        leiningen
+        htop
+        pfetch
+        crystal
+    ];
+
+    sessionVariables = {
+      PAGER = "less";
+      CLICLOLOR = 1;
+      EDITOR = "nvim";
+    };
+  };
+
+  programs = {
+    starship = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    bat = {
+      enable = true;
+      config.theme = "TwoDark";
+    };
+
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    eza.enable = true;
+    git.enable = true;
+  };
+
   home.file.".inputrc".source = ./settings/inputrc;
+  # Don't change this when you change package input. Leave it alone.
+  home.stateVersion = "23.11";
 }
